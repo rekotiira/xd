cf33366bc00d88639da0b550e73434f57785b93d
Firas <assaad.firas@gmail.com>
2012-06-27 08:45:23 +0300

    Use SFML 2.0 RC

--- src/audio/music.cpp
+++ src/audio/music.cpp
@@ -16,7 +16,7 @@ xd::music::music(const std::string& filename)
 {
 	// load music from file
 	auto handle = std::unique_ptr<detail::music_handle>(new detail::music_handle);
-	if (!handle->music.OpenFromFile(filename))
+	if (!handle->music.openFromFile(filename))
 		throw audio_file_load_failed(filename);
 	// all ok, release the memory to the real handle
 	m_handle = handle.release();
@@ -29,52 +29,52 @@ xd::music::~music()
 
 void xd::music::play()
 {
-	m_handle->music.Play();
+	m_handle->music.play();
 }
 
 void xd::music::pause()
 {
-	m_handle->music.Pause();
+	m_handle->music.pause();
 }
 
 void xd::music::stop()
 {
-	m_handle->music.Stop();
+	m_handle->music.stop();
 }
 
 bool xd::music::playing() const
 {
-	return m_handle->music.GetStatus() == sf::SoundSource::Playing;
+	return m_handle->music.getStatus() == sf::SoundSource::Playing;
 }
 
 bool xd::music::paused() const
 {
-	return m_handle->music.GetStatus() == sf::SoundSource::Paused;
+	return m_handle->music.getStatus() == sf::SoundSource::Paused;
 }
 
 bool xd::music::stopped() const
 {
-	return m_handle->music.GetStatus() == sf::SoundSource::Stopped;
+	return m_handle->music.getStatus() == sf::SoundSource::Stopped;
 }
 
 void xd::music::set_offset(float offset)
 {
-	m_handle->music.SetPlayingOffset(sf::Seconds(offset));
+	m_handle->music.setPlayingOffset(sf::seconds(offset));
 }
 
 void xd::music::set_volume(float volume)
 {
-	m_handle->music.SetVolume(volume);
+	m_handle->music.setVolume(volume);
 }
 
 void xd::music::set_pitch(float pitch)
 {
-	m_handle->music.SetPitch(pitch);
+	m_handle->music.setPitch(pitch);
 }
 
 void xd::music::set_looping(bool looping)
 {
-	m_handle->music.SetLoop(looping);
+	m_handle->music.setLoop(looping);
 }
 
 void xd::music::set_loop_point(float time)
@@ -84,22 +84,22 @@ void xd::music::set_loop_point(float time)
 
 float xd::music::get_offset() const
 {
-	return m_handle->music.GetPlayingOffset().AsSeconds();
+	return m_handle->music.getPlayingOffset().asSeconds();
 }
 
 float xd::music::get_volume() const
 {
-	return m_handle->music.GetVolume();
+	return m_handle->music.getVolume();
 }
 
 float xd::music::get_pitch() const
 {
-	return m_handle->music.GetPitch();
+	return m_handle->music.getPitch();
 }
 
 bool xd::music::get_looping() const
 {
-	return m_handle->music.GetLoop();
+	return m_handle->music.getLoop();
 }
 
 float xd::music::get_loop_point() const
--- src/audio/sound.cpp
+++ src/audio/sound.cpp
@@ -16,11 +16,11 @@ xd::sound::sound(const std::string& filename)
 {
 	// load audio buffer from file
 	auto handle = std::unique_ptr<detail::sound_handle>(new detail::sound_handle);
-	if (!handle->buffer.LoadFromFile(filename))
+	if (!handle->buffer.loadFromFile(filename))
 		throw audio_file_load_failed(filename);
 	// everything went ok, release the memory to the real handle
 	m_handle = handle.release();
-	m_handle->sound.SetBuffer(m_handle->buffer);
+	m_handle->sound.setBuffer(m_handle->buffer);
 }
 
 xd::sound::~sound()
@@ -30,53 +30,53 @@ xd::sound::~sound()
 
 void xd::sound::play()
 {
-	m_handle->sound.Play();
+	m_handle->sound.play();
 }
 
 void xd::sound::pause()
 {
-	m_handle->sound.Pause();
+	m_handle->sound.pause();
 }
 
 void xd::sound::stop()
 {
-	m_handle->sound.Stop();
+	m_handle->sound.stop();
 }
 
 
 bool xd::sound::playing() const
 {
-	return m_handle->sound.GetStatus() == sf::SoundSource::Playing;
+	return m_handle->sound.getStatus() == sf::SoundSource::Playing;
 }
 
 bool xd::sound::paused() const
 {
-	return m_handle->sound.GetStatus() == sf::SoundSource::Paused;
+	return m_handle->sound.getStatus() == sf::SoundSource::Paused;
 }
 
 bool xd::sound::stopped() const
 {
-	return m_handle->sound.GetStatus() == sf::SoundSource::Stopped;
+	return m_handle->sound.getStatus() == sf::SoundSource::Stopped;
 }
 
 void xd::sound::set_offset(float offset)
 {
-	m_handle->sound.SetPlayingOffset(sf::Seconds(offset));
+	m_handle->sound.setPlayingOffset(sf::seconds(offset));
 }
 
 void xd::sound::set_volume(float volume)
 {
-	m_handle->sound.SetVolume(volume);
+	m_handle->sound.setVolume(volume);
 }
 
 void xd::sound::set_pitch(float pitch)
 {
-	m_handle->sound.SetPitch(pitch);
+	m_handle->sound.setPitch(pitch);
 }
 
 void xd::sound::set_looping(bool looping)
 {
-	m_handle->sound.SetLoop(looping);
+	m_handle->sound.setLoop(looping);
 }
 
 void xd::sound::set_loop_point(float time)
@@ -86,22 +86,22 @@ void xd::sound::set_loop_point(float time)
 
 float xd::sound::get_offset() const
 {
-	return m_handle->sound.GetPlayingOffset().AsSeconds();
+	return m_handle->sound.getPlayingOffset().asSeconds();
 }
 
 float xd::sound::get_volume() const
 {
-	return m_handle->sound.GetVolume();
+	return m_handle->sound.getVolume();
 }
 
 float xd::sound::get_pitch() const
 {
-	return m_handle->sound.GetPitch();
+	return m_handle->sound.getPitch();
 }
 
 bool xd::sound::get_looping() const
 {
-	return m_handle->sound.GetLoop();
+	return m_handle->sound.getLoop();
 }
 
 float xd::sound::get_loop_point() const
